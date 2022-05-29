bd7fd6853e7663e9706bc9a995648256103db8f9
vikas <dongarevikas55@gmail.com>
2022-05-29 09:40:57 +0530

    Contact us page

--- frontend/src/Components/Contactus.js
+++ frontend/src/Components/Contactus.js
@@ -3,67 +3,66 @@ import React from 'react';
 function Contactus() {
     return (
         <div>
+            <div style={{ height: "200px" }}></div>
             <footer id="contactUs" class="page-footer font-small">
 
-                <div class="bg-white text-black">
-                    <div class="container  ">
-                        {/* <div class="row pt-3">
-                            <div class="col-10">
-                                <h1> Contact Us </h1>
-                                <hr> 
-                            </div>
-                        </div> */}
+                <div class="container">
+                    <div class="row pt-3 mb-5">
+                        <div class="col-12">
+                            <h1 style={{ textAlign: "center" }}> Contact Us </h1>
+                            {/* <hr> */}
+                        </div>
+                    </div>
 
 
-                        <div class="container text-center text-md-left ">
+                    <div class="container text-center text-md-left ">
 
-                            <div class="row">
+                        <div class="row">
 
-                                <div class="col-md-6 col-lg-3 col-xl-3 mx-auto mb-md-3 mb-4">
+                            <div class="col-md-6 col-lg-3 col-xl-3 mx-auto mb-md-3 mb-4">
 
-                                    <h5>Vikas Dongare</h5>
-                                    {/* <hr class="deep-purple accent-2 mb-3 mt-0 d-inline-block mx-auto" style="width: 60px;"> */}
-                                    <p>
-                                        <i class="fas fa-envelope mr-3"></i> dongarevikas55@gmail.com</p>
-                                    {/* <p>
+                                <h5>Vikas Dongare</h5>
+                                {/* <hr class="deep-purple accent-2 mb-3 mt-0 d-inline-block mx-auto" style="width: 60px;"> */}
+                                <p>
+                                    <i class="fas fa-envelope mr-3"></i> dongarevikas55@gmail.com</p>
+                                {/* <p>
                                         <i class="fas fa-phone mr-3"></i> + 91 99750 93783</p> */}
 
-                                </div>
-                                <div class="col-md-6 col-lg-3 col-xl-3 mx-auto mb-md-3 mb-4">
+                            </div>
+                            <div class="col-md-6 col-lg-3 col-xl-3 mx-auto mb-md-3 mb-4">
 
-                                    <h5>Mayur Badwaik</h5>
-                                    {/* <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;"> */}
-                                    <p>
-                                        <i class="fas fa-envelope mr-3"></i> thebadwaikmayur@gmail.com</p>
-                                    {/* <p>
+                                <h5>Mayur Badwaik</h5>
+                                {/* <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;"> */}
+                                <p>
+                                    <i class="fas fa-envelope mr-3"></i> thebadwaikmayur@gmail.com</p>
+                                {/* <p>
                                         <i class="fas fa-phone mr-3"></i> + 91 95038 93010</p> */}
 
-                                </div>
-                                <div class="col-md-6 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                            </div>
+                            <div class="col-md-6 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
 
-                                    <h5>Khushal Bendkoli</h5>
-                                    {/* <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;"> */}
-                                    <p>
-                                        <i class="fas fa-envelope mr-3"></i> @gmail.com</p>
-                                    {/* <p>
+                                <h5>Khushal Bendkoli</h5>
+                                {/* <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;"> */}
+                                <p>
+                                    <i class="fas fa-envelope mr-3"></i> bendkolikhushal10@gmail.com</p>
+                                {/* <p>
                                         <i class="fas fa-phone mr-3"></i> + 91 96232 17110</p> */}
 
-                                </div>
+                            </div>
 
-                                <div class="col-md-6 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                            <div class="col-md-6 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
 
-                                    <h5>Parnavi Joshi</h5>
-                                    {/* <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;"> */}
-                                    <p>
-                                        <i class="fas fa-envelope mr-3"></i> @gmail.com</p>
-                                    {/* <p>
+                                <h5>Parnavi Joshi</h5>
+                                {/* <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;"> */}
+                                <p>
+                                    <i class="fas fa-envelope mr-3"></i> parnavijoshi00@gmail.com</p>
+                                {/* <p>
                                         <i class="fas fa-phone mr-3"></i> + 91 88887 17513</p> */}
-
-                                </div>
 
                             </div>
 
                         </div>
+
                     </div>
                 </div>
             </footer>
